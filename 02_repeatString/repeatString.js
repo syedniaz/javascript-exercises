938aa2c81b9aaea5dd87b66636cfff8a4100e08c
syedniaz <snm051110@gmail.com>
2024-01-08 20:31:17 +0600

    04 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    let str1 = "";
+    for (let i=0; i < num; i++){
+        str1 = str1.concat(str);
+    }
+    return str1;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // Iterate over each argument
+    args.forEach(arg => {
+        // Find the index of the argument in the array
+        let index = arr.indexOf(arg);
+        
+        // Check if the argument is found in the array
+        if (index !== -1) {
+            arr.splice(index, 1); // Use splice to remove the element at the found index
+        }
+    });
 
+    return arr;
 };
 
 // Do not edit below this line
