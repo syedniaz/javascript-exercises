3ddd6d2adc4ac51b49410318e7ecb28f0755ed0c
syedniaz <snm051110@gmail.com>
2024-01-11 02:28:36 +0600

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(...num) {
+  let sum = 0;
+	num.forEach(nums => {
+    sum += nums;
+  });
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	let sum = 0;
+	num.forEach(nums => {
+    sum += nums;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let mul = 1;
+	num.forEach(nums => {
+    mul *= nums;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for(let i = num; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
