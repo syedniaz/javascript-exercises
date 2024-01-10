c3c4a68141f6e3c3166c11f6db8104ad9c48ea3f
syedniaz <snm051110@gmail.com>
2024-01-10 22:46:20 +0600

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let new_temp = (temp - 32) * (5/9);
+  new_temp = Math.round(new_temp * 10) / 10;
+  return new_temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let new_temp = temp * (9/5) + 32;
+  new_temp = Math.round(new_temp * 10) / 10;
+  return new_temp;
 };
 
 // Do not edit below this line
