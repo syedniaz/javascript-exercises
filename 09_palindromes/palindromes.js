9c891adfbad27c0f8bf93f55ea85c2b5991ea9cd
syedniaz <snm051110@gmail.com>
2024-01-11 15:43:46 +0600

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z0-9]/g, "");
+    let reverseStr = str.split("").reverse().join("");
+    return str === reverseStr;
 };
 
 // Do not edit below this line
