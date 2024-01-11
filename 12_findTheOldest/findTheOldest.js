2a5d5b6f32769706e0a568069fc59c0eeed9753e
syedniaz <snm051110@gmail.com>
2024-01-11 20:21:10 +0600

    11 and 12 done, completed all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+        if (person.age > oldestPerson.age) {
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
