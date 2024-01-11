0ee26258bd5566a06e91d8a163bc32d3829ea4aa
syedniaz <snm051110@gmail.com>
2024-01-11 20:11:27 +0600

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (typeof n == "string") {
+        n = parseInt(n);
+    }
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
